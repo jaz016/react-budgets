6713e76e5133efc9e330629755750e86768ad8e8
Jasper Bautista <xjet16@gmail.com>
2023-06-06 19:17:22 +0800

    modified money util for handling negative values

--- src/utils/money.js
+++ src/utils/money.js
@@ -1,10 +1,12 @@
 export function formatMoney(number) {
 	// Convert the number to a floating-point number with 2 decimal places
+	let isNegative = false;
 	let formattedAmount = number.toFixed(2);
+	isNegative = formattedAmount < 0;
 
 	// Add commas for thousands separator
-	formattedAmount = formattedAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	formattedAmount = "P" + formattedAmount;
+	formattedAmount = Math.abs(formattedAmount).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	formattedAmount = `${isNegative ? '-P' : 'P'}` + formattedAmount;
   
 	return formattedAmount;
 }
