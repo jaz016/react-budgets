ba61db0db2f4acc5122378ed6727a243c16ff496
Jasper Bautista <xjet16@gmail.com>
2023-06-08 21:41:14 +0800

    utilize dayjs in datetime util

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 export function formatDateTime(date) {
 	const options = {
 		month: '2-digit',
@@ -13,23 +15,28 @@ export function formatDateTime(date) {
 }
 
 export function formatISOToYMD(ISOStr) {
-	const date = new Date(ISOStr);
-	const yyyy = date.getUTCFullYear();
-	const mm = String(date.getUTCMonth() + 1).padStart(2, '0');
-	const dd = String(date.getUTCDate()).padStart(2, '0');
-	const formattedDate = `${yyyy}-${mm}-${dd}`;
+	// const date = new Date(ISOStr);
+	// const yyyy = date.getUTCFullYear();
+	// const mm = String(date.getUTCMonth() + 1).padStart(2, '0');
+	// const dd = String(date.getUTCDate()).padStart(2, '0');
+	// const formattedDate = `${yyyy}-${mm}-${dd}`;
 
-	return formattedDate;
+	// return formattedDate;
+
+	return dayjs(ISOStr).format('YYYY-MM-DD');
+
 }
 
 export function formatISOtoFullDt(ISOStr) {
-	const date = new Date(ISOStr); // Convert to Date object
-	const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 
-						'July', 'August', 'September', 'October', 'November', 'December'];
-	const month = monthNames[date.getMonth()]; // Get month name
-	const day = date.getDate(); // Get day
-	const year = date.getFullYear(); // Get year
-	const formattedDate = `${month} ${day}, ${year}`; // Combine into desired format
+	// const date = new Date(ISOStr); // Convert to Date object
+	// const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 
+	// 					'July', 'August', 'September', 'October', 'November', 'December'];
+	// const month = monthNames[date.getMonth()]; // Get month name
+	// const day = date.getDate(); // Get day
+	// const year = date.getFullYear(); // Get year
+	// const formattedDate = `${month} ${day}, ${year}`; // Combine into desired format
 
-	return formattedDate;
+	// return formattedDate;
+
+	return dayjs(ISOStr).format('MMMM D, YYYY')
 }
