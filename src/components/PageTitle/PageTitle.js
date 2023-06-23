24c75f2ee847638051cb043398c6d619551c189a
Jasper Bautista <xjet16@gmail.com>
2023-06-23 23:26:24 +0800

    updated Go back button to redirect to prev history

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Typography, Button } from "@mui/material";
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import Grid from '@mui/material/Grid';
 
 function PageTitle({title}) {
+	const navigate = useNavigate();
+
 	return (
 		<Grid container>
 			<Grid item md={12} sx={{display: 'flex', justifyContent: 'space-between'}}>
 				<Typography variant='h4' component='h1' sx={{fontWeight: 'lighter', display: 'inline-block'}}>{title}</Typography>
-				<Button component={Link} to='/' variant="outlined" startIcon={<KeyboardArrowLeftIcon />}>
+				<Button onClick={() => navigate(-1)} variant="outlined" startIcon={<KeyboardArrowLeftIcon />}>
 					Go back
 				</Button>
 			</Grid>
